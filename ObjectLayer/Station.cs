1e854f96bfa618228af1538ccf8ca1636348d566
Jack Millar <99406899+JackoMillar@users.noreply.github.com>
2024-06-27 02:15:51 +0100

    adding in the datalayer files and objectlayer
    excluding obj file as its got too many files for githubs liking

---
+++ ObjectLayer/Station.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Tube.ObjectLayer
+{
+
+    /*
+     Station Class that holds details of a metro station as well as a method to display the details of a station
+   */
+
+    public class Station
+    {
+
+        // Create private variables
+        private string id;
+        private string name;
+        private int zone;
+        private int started;
+        private int ended;
+       
+        // Create a public string method for name, line and ID with getters and setters
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Zone
+        {
+            get { return zone; }
+            set { zone = value; }
+        }
+
+        public int Started
+        {
+            get { return started; }
+            set { started = value; }
+        }
+
+        public int Ended
+        {
+            get { return ended; }
+            set { ended = value; }
+        }
+        // ToString to return station objects data
+        public override string ToString()
+        {
+            return id + ", " + name + ", zone " + zone + ". " + started + ", " + ended;
+        }
+    }
+}
---
+++ ObjectLayer/Underground.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Tube.ObjectLayer
+{
+
+      public class Underground
+    {
+        // Init List variable to store multiple stations
+        Dictionary<string, Station> stations = new Dictionary<string, Station>();
+
+        // Stop the object being instantiated using new
+        private Underground(){}
+        // Holds the reference to the only instance of singleton
+        private static Underground? instance;
+
+        public static Underground Instance
+        {
+            // Used instead of a construtor, creates only one instance
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Underground(); 
+                }
+                return instance;
+            }
+        }
+
+        // Method to add station to list variable and carry along to station class 
+        public void AddStation(Station station)
+        {
+            stations[station.Id] = station;
+        }
+
+        // Method to clear the stations dictionary 
+        public void Clear()
+        {
+            stations.Clear();
+        }
+
+        // Method to find station based of id
+        public Station Find(string id)
+        {
+            if (stations.TryGetValue(id, out Station station))
+            {
+                return station;
+            }
+
+            return null;
+        }
+
+        // Method to print every station in Underground
+        public override string ToString()
+        {
+                return string.Join("\r\n", stations.Values);  
+        }
+    }
+}
